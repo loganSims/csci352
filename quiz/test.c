db2bccf668726b1805a38997c19b2f4742cb7bff
simsl <logan.sims94@gmail.com>
2015-07-12 13:05:17 -0700

    standard commit

---
+++ quiz/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define thing(X) #X
+
+#define BUF_SIZE 1024
+#define DEBUG 1
+ 
+/*
+fref.c using the main function to open and read files.
+It works like a wrapper for the match function 
+to find regular expressions in files.
+*/
+int main(int argc, char *argv[]){
+
+  printf("hello\n");
+
+  return 0;
+}
