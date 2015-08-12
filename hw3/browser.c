439cdee880d722ae78d58956effec86ca4707c35
simsl <logan.sims94@gmail.com>
2015-08-11 23:05:22 -0700

    now builds tree as items are clicked, crashes when weird file types are selected

--- hw3/browser.c
+++ hw3/browser.c
@@ -10,14 +10,13 @@ static void display (GtkWidget* hpaned);
 static void build_dirstore(GtkListStore *dirstore, char *path);
 static void build_dirview(GtkWidget *dirview);
 
-static int build_sysstore(GtkTreeStore *treestore, char *path,
-                          GtkTreeIter *iter, int level);
+static int build_sysstore(GtkTreeStore *treestore, GtkTreeIter *iter, char *path, int is_root);
 
 static void build_sysview(GtkWidget *sysview, GtkWidget *dirview);
 
-void update_dir(GtkTreeSelection *selection, GtkWidget *dirview);
+void sys_item_selected(GtkTreeSelection *selection, GtkWidget *dirview);
 
-void dir_item_selected (GtkWidget *selection, gpointer data);
+void dir_item_selected(GtkWidget *selection, gpointer data);
 
 struct Ddata {
   char *path;
@@ -53,7 +52,6 @@ int main (int argc, char *argv[]){
   gtk_init (&argc, &argv);
 
   //filler
-  //GtkWidget *button1 = gtk_button_new_with_label ("sys");
   GtkWidget *button2 = gtk_button_new_with_label ("hex");
   //end filler
 
@@ -82,8 +80,9 @@ int main (int argc, char *argv[]){
                                               GDK_TYPE_PIXBUF,
                                               G_TYPE_INT);
 
-  GtkTreeIter iter[MAX_DEPTH];
-  build_sysstore(sysstore, start_path, iter, 0);
+  GtkTreeIter iter;
+
+  build_sysstore(sysstore, &iter, start_path, 1);
 
   // setup sysview
   GtkWidget *sysview = gtk_tree_view_new();
@@ -155,37 +154,40 @@ static void build_dirstore(GtkListStore *dirstore, char *path){
 
   while((entry = readdir(dp))){
 
-    // Get path of entry for stat
-    snprintf(entrypath, MAX_DIRLEN, "%s%s", path, entry->d_name);
-    stat(entrypath, &sb);
+    if (entry->d_name[0] != '.'){   
 
-    // Format time
-    localtime_r(&(sb.st_mtime), &time);
-    strftime(modtime, sizeof(modtime), "%D", &time);
+      // Get path of entry for stat
+      snprintf(entrypath, MAX_DIRLEN, "%s%s", path, entry->d_name);
+      stat(entrypath, &sb);
+
+      // Format time
+      localtime_r(&(sb.st_mtime), &time);
+      strftime(modtime, sizeof(modtime), "%D", &time);
     
-    // assemble permission string
-    makePerm(&sb, permission);
+      // assemble permission string
+      makePerm(&sb, permission);
 
-    // format size
-    snprintf(size, 15, "%.1f %s", 
-        ((sb.st_size < 1000) ? sb.st_size : (sb.st_size/1000.0)),
-        ((sb.st_size < 1000) ? "B" : "KiB") );
-    gtk_list_store_append (dirstore, &iter);
+      // format size
+      snprintf(size, 15, "%.1f %s", 
+               ((sb.st_size < 1000) ? sb.st_size : (sb.st_size/1000.0)),
+               ((sb.st_size < 1000) ? "B" : "KiB") );
+      gtk_list_store_append (dirstore, &iter);
 
-    // setup icon  TODO c file, exe file, ...
-    switch (sb.st_mode & S_IFMT) {
-      case S_IFDIR: icon = dir_icon; break; //directory
-      case S_IFREG: icon = file_icon; break; //reg file
-      default:  icon = unknown_icon; break; //unknown
+      // setup icon  TODO c file, exe file, ...
+      switch (sb.st_mode & S_IFMT) {
+        case S_IFDIR: icon = dir_icon; break; //directory
+        case S_IFREG: icon = file_icon; break; //reg file
+        default:  icon = unknown_icon; break; //unknown
+      }
+
+      gtk_list_store_set (dirstore, &iter,
+                          NAME_COLUMN, entry->d_name,
+                          SIZE_COLUMN, size,
+                          DATE_COLUMN, modtime,
+                          PERM_COLUMN, permission, 
+                          ICON_COLUMN, icon, -1);
+
     }
-
-    gtk_list_store_set (dirstore, &iter,
-                        NAME_COLUMN, entry->d_name,
-                        SIZE_COLUMN, size,
-                        DATE_COLUMN, modtime,
-                        PERM_COLUMN, permission, 
-                        ICON_COLUMN, icon, -1);
-
   }
 
 }
@@ -257,14 +259,17 @@ void dir_item_selected (GtkWidget *selection, gpointer data) {
 }
 
 
-void update_dir(GtkTreeSelection *selection, GtkWidget *dirview){
+void sys_item_selected(GtkTreeSelection *selection, GtkWidget *dirview){
 
   GtkTreeModel *model;
+  GtkTreeIter iter;
   GtkTreeIter child;
   GtkTreeIter parent;
   gchar *dirname;
   char path[MAX_DIRLEN];
   char newpath[MAX_DIRLEN];
+
+  // Part (a) build path
 
   if (gtk_tree_selection_get_selected (GTK_TREE_SELECTION(selection), 
                                        &model, &child)) {
@@ -285,6 +290,10 @@ void update_dir(GtkTreeSelection *selection, GtkWidget *dirview){
 
     }
 
+    g_message("%s\n", path);
+
+    // Part (b) build new dir store
+
     GtkListStore *dirstore = gtk_list_store_new(N_COLUMNS,
                                                 G_TYPE_STRING, /*File name   */
                                                 G_TYPE_STRING, /*File size   */
@@ -301,53 +310,65 @@ void update_dir(GtkTreeSelection *selection, GtkWidget *dirview){
                              GTK_TREE_MODEL (dirstore));
 
     g_object_unref(dirstore);
-  
+
+    // Part (c) update sys tree 
+
+    if (gtk_tree_selection_get_selected (GTK_TREE_SELECTION(selection), 
+                                         &model, &iter)) {
+      build_sysstore(GTK_TREE_STORE(model), &iter, path, 0);
+
+    }  
   }
 }
 
-static int build_sysstore(GtkTreeStore *treestore, char *path, GtkTreeIter *iter, int level){
+
+//iter is already in tree, adding its children
+static int build_sysstore(GtkTreeStore *sysstore, GtkTreeIter *iter, char *path, int is_root){
   
   DIR *dp;
   struct stat sb;
   struct dirent *entry;
+
+  GtkTreeIter child;
+
   GError *error = NULL;
   GdkPixbuf* dir_icon = gdk_pixbuf_new_from_file("dir_icon.png", &error);
 
-  dp = opendir(path);
- 
-  if (dp == NULL){
-    return -1;
-  }
 
-  if (level == 0){
-    gtk_tree_store_append (treestore, &(iter[level]), NULL);
-    gtk_tree_store_set (treestore, &(iter[level]), 
+  if (is_root){
+    gtk_tree_store_append (sysstore, iter, NULL);
+    gtk_tree_store_set (sysstore, iter, 
                         TNAME_COLUMN, "home",
-                        TICON_COLUMN, dir_icon,
-                        TLEVEL, level, -1);
-    level++;
+                        TICON_COLUMN, dir_icon, -1);
   }
 
-  char entrypath[MAX_DIRLEN];
+  // Check if iter hasn't gotten it's children yet
+  if (gtk_tree_model_iter_has_child(GTK_TREE_MODEL(sysstore), iter) != TRUE){
 
-  while((entry = readdir(dp))){
-    snprintf(entrypath, MAX_DIRLEN, "%s%s", path, entry->d_name);
-    stat(entrypath, &sb);
-    if(S_ISDIR(sb.st_mode)){
-      if (entry->d_name[0] != '.'){   
-        gtk_tree_store_append (treestore, &(iter[level]), &(iter[level-1]));
+    dp = opendir(path);
+ 
+    if (dp == NULL){
+      return -1;
+    }
+
+    char entrypath[MAX_DIRLEN];
+
+    while((entry = readdir(dp))){
+      snprintf(entrypath, MAX_DIRLEN, "%s%s", path, entry->d_name);
+      stat(entrypath, &sb);
+      if(S_ISDIR(sb.st_mode)){
+        if (entry->d_name[0] != '.'){   
+          gtk_tree_store_append (sysstore, &child, iter);
             
-        gtk_tree_store_set (treestore, &(iter[level]), 
-                            TNAME_COLUMN, entry->d_name,
-                            TICON_COLUMN, dir_icon, -1);
-     
-        snprintf(entrypath, MAX_DIRLEN, "%s%s/", path, entry->d_name);
-        build_sysstore(treestore, entrypath, iter, (level+1));  
+          gtk_tree_store_set (sysstore, &child, 
+                              TNAME_COLUMN, entry->d_name,
+                              TICON_COLUMN, dir_icon, -1);
+        }
       }
     }
+    closedir(dp);
   }
 
-  closedir(dp);
   return 0;
 }
 static void build_sysview(GtkWidget *sysview, GtkWidget *dirview){
@@ -376,7 +397,7 @@ static void build_sysview(GtkWidget *sysview, GtkWidget *dirview){
 
     // connect the selection callback function
     g_signal_connect(G_OBJECT(selection), "changed",
-                     G_CALLBACK(update_dir), dirview);
+                     G_CALLBACK(sys_item_selected), dirview);
 
 }
 
